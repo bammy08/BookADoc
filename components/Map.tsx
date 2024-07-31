987aefbc265e1ce302e29372cdfc94caeeedbf7c
bammy08 <olusolabamidele33@gmail.com>
2024-07-31 09:31:39 +0100

    map error fixed

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,35 +2,62 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Button, Box, Flex } from '@chakra-ui/react';
 import L from 'leaflet';
 import { useEffect } from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
-const Map: React.FC = () => {
+const Map = () => {
   useEffect(() => {
-    // Ensure this runs only on the client side
     if (typeof window !== 'undefined') {
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl:
-          'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-        iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-        shadowUrl:
-          'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-      });
+      // Define new icon URLs
+      const iconRetinaUrl =
+        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png';
+      const iconUrl =
+        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png';
+      const shadowUrl =
+        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png';
+
+      // Create a new icon class with these URLs
+      const iconDefault = new L.Icon.Default();
+      iconDefault.options.iconRetinaUrl = iconRetinaUrl;
+      iconDefault.options.iconUrl = iconUrl;
+      iconDefault.options.shadowUrl = shadowUrl;
+
+      // Set the default icon class
+      L.Marker.prototype.options.icon = iconDefault;
     }
   }, []);
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} className="h-96 w-full">
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <Box className="bg-white p-4 rounded-lg shadow-md">
+      <Box className="relative">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          className="h-64 w-full rounded-lg"
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </Box>
+      <Flex justifyContent="center" mt={4}>
+        <Button
+          leftIcon={<FaMapMarkerAlt />}
+          colorScheme="pink"
+          variant="outline"
+        >
+          View in a map
+        </Button>
+      </Flex>
+    </Box>
   );
 };
 
